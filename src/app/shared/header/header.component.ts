7cc6a5af329a3ad184f1903d5a247b746c2da03a
Hemant Basnet <hemantbasnet61@gmil.com>
2023-07-19 14:28:25 +0545

    Link the Navitem with their  link

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  navList: Array<any> = [
+    {
+      navLink: 'events',
+      NavName: 'Upcoming Events',
+    },
 
+    {
+      navLink: 'blogs',
+      NavName: 'Blogs',
+    },
+    {
+      navLink: 'aboutus',
+      NavName: 'Aboutus',
+    },
+    {
+      navLink: 'contactus',
+      NavName: 'Contact Us',
+    },
+
+    {
+      navLink: 'project',
+      NavName: 'Our Works',
+    },
+    {
+      navLink: '',
+      NavName: 'Home',
+    },
+  ];
 }
