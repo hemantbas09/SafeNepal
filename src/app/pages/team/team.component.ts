0297f2e4dcd196d9daf16ffe9b7f289412e1d37c
Hemant Basnet <hemantbasnet61@gmil.com>
2023-07-24 15:02:06 +0545

    Team link

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -11,26 +11,31 @@ export class TeamComponent {
       image: 'assets/images/team/team-1.jpg',
       name: 'Samrat Ghimire',
       position: 'President',
+      link: 'https://www.facebook.com/ghimiresamrat1',
     },
     {
       image: 'assets/images/team/team-2.jpg',
       name: 'Sanskar Raj Subedi',
       position: 'Vice-President',
+      link: 'https://www.facebook.com/sanskarsubedi11',
     },
     {
       image: 'assets/images/team/team-3.jpg',
       name: 'Sachin Bikram Basnet',
       position: 'Secretary',
+      link: 'https://www.facebook.com/Sachin.Bikram.Basnet',
     },
     {
       image: 'assets/images/team/team-4.jpg',
       name: 'Gayatri Bhattarai',
       position: 'Vice-Secretary',
+      link: 'https://www.facebook.com/gauri.bhattarai.731',
     },
     {
       image: 'assets/images/team/team-5.jpg',
       name: 'Reetisha Basel',
       position: 'Treasurer',
+      link: 'https://www.facebook.com/ritisha.basel',
     },
   ];
 }
